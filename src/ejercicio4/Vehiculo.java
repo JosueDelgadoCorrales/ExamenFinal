27a1b68e75067bc8b3cece5b37303022274b1cf2
JosueDelgadoCorrales <josuecodelgado@gmail.com>
2023-11-24 15:55:03 -0600

    Se editó el examen

---
+++ src/ejercicio4/Vehiculo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author josue
+ */
+public class Vehiculo {
+    String marca;
+    String color;
+    
+    public void Acelerar(){
+        
+    }
+    
+    public void Frenar(){
+        
+    }
+}
